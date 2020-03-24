5c29b6a888193c1fb3c57df3513d4fd438dfb4b3
captainspockears <ruthuparna1998@gmail.com>
2020-03-24 20:36:40 +0530

    Got the correct sigmoid function

--- Week-3/machine-learning-ex2/ex2/sigmoid.m
+++ Week-3/machine-learning-ex2/ex2/sigmoid.m
@@ -2,16 +2,6 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
 
-% Flag to check if z is a vector
-VECTORFLAG = 0;
-
-if size(z,2)==1,
-
-    VECTORFLAG = 1;
-    z = z';
-
-end
-
 % You need to return the following variables correctly 
 g = zeros(size(z));
 
@@ -19,16 +9,12 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-for i = 1:size(z,2),
+for i = 1:size(z,1),
+    for j = 1:size(z,2),
 
-    g(i) = 1 / (1 + exp(-1 * z(i)));
+        g(i,j) = 1 / (1 + exp(-1 * z(i,j)));
 
-end
-
-if VECTORFLAG==1,
-
-    g = g';
-
+    end
 end
 
 % =============================================================
