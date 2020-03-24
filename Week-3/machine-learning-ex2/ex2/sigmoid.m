cd4abfcdd9cbd62d694b869f812396a5ef629354
captainspockears <ruthuparna1998@gmail.com>
2020-03-24 19:58:36 +0530

    Hardcoded vector implimentation

--- Week-3/machine-learning-ex2/ex2/sigmoid.m
+++ Week-3/machine-learning-ex2/ex2/sigmoid.m
@@ -2,6 +2,16 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
 
+% Flag to check if z is a vector
+VECTORFLAG = 0;
+
+if size(z,2)==1,
+
+    VECTORFLAG = 1;
+    z = z';
+
+end
+
 % You need to return the following variables correctly 
 g = zeros(size(z));
 
@@ -9,9 +19,17 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+for i = 1:size(z,2),
 
+    g(i) = 1 / (1 + exp(-1 * z(i)));
 
+end
 
+if VECTORFLAG==1,
+
+    g = g';
+
+end
 
 % =============================================================
 
